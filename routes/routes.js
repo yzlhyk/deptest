c2c17c45dd01edadd5d79e100d18072c8bce3449
yzlhyk <yzlhyk@gmail.com>
2021-03-14 01:15:34 +0800

    ready for deploy

---
+++ routes/routes.js
@@ -0,0 +1,8 @@
+
+const express = require('express');
+const router = express.Router();
+const controllers = require('./../controllers/controllers');
+
+router.get('/say-something', controllers.saySomething);
+
+module.exports = router;
